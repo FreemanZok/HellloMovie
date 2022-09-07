fc8c0ca99a90952b591c4415f8a2758651d255a7
ali zokaei <ali.zokaei.1367@gmail.com>
2022-09-08 01:49:57 +0430

    last change before images/

---
+++ src/components/littleMovie.vue
@@ -0,0 +1,90 @@
+
+<template>
+    <div class="littleMovie">
+        <div class="my-card my-curve my-container ">
+            <div class="background my-wrapper go-left clearfix">
+                <div class="left-card">
+                    <div class="img-wrapper">
+                        <img :src='require(`@/assets/img/${my_src}`)' alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'littleMovie',
+    props: ["my_src"],
+    computed: {
+       
+    },
+    methods:{
+        
+    }
+}
+</script>
+  
+
+<style lang="scss" scoped>
+.clearfix {
+    overflow: auto;
+}
+
+.clearfix::after {
+    content: "";
+    clear: both;
+    display: table;
+}
+
+.img-wrapper {
+    overflow: hidden;
+    height: 150px;
+}
+
+.left-card {
+    float: left;
+    width: 50%;
+
+    img {
+        width: 100%;
+        height: 100%;
+        max-width: 120px;
+    }
+}
+
+.littleMovie {
+    .go-left {
+        text-align: left;
+    }
+
+    .my-card {
+
+        min-height: 30px;
+        padding: 8px 20px;
+
+        &.my-curve {
+            border-radius: 10px;
+        }
+    }
+}
+
+.my-container {
+    max-width: 1024px;
+    margin: 0 auto;
+}
+
+.my-wrapper {
+    // padding: 17px 25px;
+    box-shadow: 0 0 4px #a0a0a0;
+    border-radius: 17px;
+    background: #e8e8e8;
+}
+
+.background {
+    background: #e8e8e8;
+}
+</style>
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,59 @@
 <template>
-  <div class="home">
-    <HeaderSearch />
+  <div class="home my-container">
+    <HeaderSearch class="go-bottom" />
+    <div class="home-body">
+      <div v-for="bab in 9" :key="bab" class="box-part">
+        <littleMovie :my_src="my_src" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import HeaderSearch from "../components/HeaderSearch.vue"
+import HeaderSearch from "../components/HeaderSearch.vue"
+import littleMovie from "../components/littleMovie.vue"
 export default {
   name: 'Home',
   components: {
-    HeaderSearch:HeaderSearch
+    HeaderSearch,
+    littleMovie,
+  },
+  data(){
+    return{
+      my_src:"61n-olilSdL._AC_SY679_.jpg"
+    }
   }
 }
 </script>
+<style lang="scss" scoped>
+.home-body{
+  margin: 0 -22px;
+}
+.clearfix {
+  overflow: auto;
+}
+
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.my-container {
+  max-width: 1024px;
+  margin: 0 auto;
+}
+
+.my-flex {
+  display: flex;
+}
+
+.go-bottom {
+  margin-bottom: 20px;
+}
+
+.box-part {
+  width: calc(100% /3);
+  float: left;
+}
+</style>
